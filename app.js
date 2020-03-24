9de848104d1bd70b2715dfaa7bb0079846ecea23
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2020-03-24 18:45:55 +0100

    Setup for my blog app

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var mongoose = require("mongoose");
+var multer = require('multer');
+var flash = require('connect-flash');
+
+
+//mongoose setup
+mongoose.connect("mongodb://localhost/blogapp",{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+app.use(bodyParser.urlencoded({extended : true})) ;
+//Express session
+app.use(session({
+	secret: "Besos",
+	resave: "false",
+	saveUninitialized: "false"
+}));
+
+//set up view engine
+app.set('view engine', 'ejs');
+//setup file uploads
+app.use(multer({dest:'./uploads'}).single('photo'));
+app.use(express.static(__dirname + "/public/images/uploads"));
+//setup flash
+app.use(flash());
+
+app.listen(2000, function(){
+	console.log("Blogapp has started");
+});
+
+
